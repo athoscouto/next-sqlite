21a354e953437c47eb1c05d8f40e0c17eef48067
Athos Couto <athos@turso.tech>
2024-07-04 13:51:37 -0300

    Replace query with SELECT 1;

--- app/api/route.js
+++ app/api/route.js
@@ -3,7 +3,7 @@ import { db } from "../sqlite";
 export async function GET() {
   const table = "student";
   const { rows } = await db.execute(
-    `SELECT * from ${table} ORDER BY random() LIMIT 10`,
+    `SELECT 1;`,
   );
   return Response.json(rows, {
     headers: {
--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import { db } from "./sqlite";
 export default async function Home() {
   const table = "student";
   const { rows } = await db.execute(
-    `SELECT * from ${table} ORDER BY random() LIMIT 10`,
+    `SELECT 1;`,
   );
   return <pre>{JSON.stringify(rows, null, 2)}</pre>;
 }
